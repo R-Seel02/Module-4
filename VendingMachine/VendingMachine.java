a6e4b76f401a75ba0ffda0a540f3b2df542d3e74
PhilipN27 <pan97g@gmail.com>
2024-02-18 17:38:41 -0500

    new vending machine class that displays snacks but does not have selection.

---
+++ VendingMachine/VendingMachine.java
@@ -0,0 +1,15 @@
+package VendingMachine;
+
+public class VendingMachine
+{
+    public static void main(String[] args) 
+    {
+        //Display snack options
+        System.out.println("Snack Options:");
+        System.out.println("1.Lebron Ruffles");
+        System.out.println("2.Honey Bun");
+        System.out.println("3.Natures Valley Bar");
+        System.out.println("4.Welch's Gummies");
+        System.out.println("5. Oreos");
+    }
+}
