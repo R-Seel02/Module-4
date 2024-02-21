f4cb0601c6d003e3915bf1ea69599df686edc0e2
PhilipN27 <pan97g@gmail.com>
2024-02-20 20:40:56 -0500

    bills are now accepted and change is given back to the user.

--- VendingMachine/VendingMachine.java
+++ VendingMachine/VendingMachine.java
@@ -23,6 +23,8 @@ public class VendingMachine
         double nvBar = 2.00;
         double wGummies = 1.50;
         double oreos = 2.25;
+
+        double snackPrice = 0.0;
         
 
         System.out.println("Which Snack would you like? ");
@@ -34,18 +36,23 @@ public class VendingMachine
         switch (snackChoice) {
             case ("Lebron Ruffles"):
             System.out.println("That will cost " + ruffPrice);
+                snackPrice = ruffPrice;
                 break;
             case("Honey Bun"):
             System.out.println("That will cost " + honeyBun);
+                snackPrice = honeyBun;
                 break;
             case ("Natures Valley Bar"):
             System.out.println("That will cost " + nvBar);
+                snackPrice = nvBar;
                 break;
             case ("Welches Gummies"):
             System.out.println("That will cost " + wGummies);
+                snackPrice = wGummies;
                 break;
             case ("Oreos"):
             System.out.println("That will cost " + oreos);
+                snackPrice = oreos;
                 break;
             default:
             System.out.println("Invalid choice, please select from the provided options.");
@@ -68,9 +75,44 @@ public class VendingMachine
             System.out.println("Error, Please enter yes or no");
         }
 
-        scan.close();
+        double totalInserted = 0;
+        System.out.println("Please insert $1 bills. Type '0' when you are done");
+        while (true)
+        {
+            System.out.println("Please insert $1 bill:");
+            int moneyInserted = scan.nextInt();
+            if (moneyInserted == 0)
+            {
+                break;
+            }
+            else if (moneyInserted == 1)
+            {
+                totalInserted += moneyInserted;
+                System.out.println("Total inserted: $" + totalInserted);
+            }
+            else
+            {
+                System.out.println("Invalid amount. Only $1 bills are accepted,");
+            }
+        }
 
-        
+
+        //Check if enough money was inserted and calculate change
+        if (totalInserted >= snackPrice)
+        {
+            System.out.println("Thank you for your purchase. Dispensing the snack. ");
+            double change = totalInserted - snackPrice;
+            if (change > 0)
+            {
+                System.out.println("Please take your change: $" + change);
+            }
+            else
+            {
+                System.out.println("Not enough money inserted. Please take back your money: $" +totalInserted);
+            }
+        }
+
+        scan.close();
 
     }
     
